c38ef80a00a5de0e66da9c97a54ecc69fd59d3d0
Art Chen <zanchoret@gmail.com>
2020-05-03 14:45:47 -0700

    A big change
    - Change theme name to Memory.
    - Migrate less to scss.
    - Support sidebar.
    - Support Table of Content.

--- README.md
+++ README.md
@@ -1,54 +1,48 @@
-# Hexo Theme Typescript
+# Hexo Theme Memory
 
-Typescript is a minimal theme for [Hexo](http://hexo.io).
+Memory is a minimal theme for [Hexo](http://hexo.io).
 
 This theme is also available on:
 
-* Ghost version: [ghost-theme-typescript](https://github.com/artchen/ghost-theme-typescript)
+* Ghost version: [ghost-theme-memory](https://github.com/artchen/ghost-theme-memory)
 
 ## Dependencies
 
-Recommended list of plugins:
+Install these Hexo plugins to unlock the full potential of this theme:
 
-* hexo-all-minifier
-* hexo-autoprefixer
-* hexo-front-matter
 * hexo-generator-archive
-* hexo-generator-category
-* hexo-generator-feed
-* hexo-generator-index
 * hexo-generator-tag
-* hexo-pagination
-* hexo-renderer-less
 * hexo-renderer-ejs
+* hexo-renderer-scss
 * hexo-renderer-marked
+* hexo-pagination
+* hexo-toc
 
 ## Customization
 
-Typescript is customizable via `_config.yml` file under the theme directory.
+The theme is customizable via `_config.yml` file under the theme directory.
 
 The global `_config.yml` for Hexo may also need customization. In particular:
 
 * Set `disqus_shortname` field to your disqus short name.
-* Set `theme` field to `hexo-theme-typescript`.
+* Set `theme` field to `hexo-theme-memory`.
 
 In addition to these settings, users may also want to edit/replace the following files:
 
-* Replace the site logo: `source/images/logo.png`, `source/images/logo.psd`
+* Replace the site logo: `source/img/logo.png`, `source/img/logo.psd`
 * The icon fonts are from [icomoon](https://icomoon.io/).
-* The default English font is Futura PT via Adobe Typekit. If you are using Typekit like me, please change the embedded javascript code in `layout/_partial/head.ejs`, else you can delete the corresponding code.
 
-This theme currently support 3 search services:
+This theme supports the following search services:
 
 * Google custom search
 * Algolia search
 * Microsoft Azure search
 
-For information about how to setup searching, please refer to another project of mine, [universal-search](https://github.com/artchen/universal-search).
+Refer to [universal-search](https://github.com/artchen/universal-search) series to learn how to set up search.
 
 ## Demo
 
-![Typescript Demo](http://artifact.me/images/ghost-theme-typescript-screenshot.png)
+![Memory Demo](https://cdn.otakism.com/assets/hexo-theme-memory/demo/ghost-theme-memory-screenshot.jpg)
 
 ## Copyright
 
@@ -60,7 +54,7 @@ Public resources used in this theme:
 
 Copyright © Art Chen
 
-Please do not remove the "Theme Typescript designed by Art Chen" text and links.
+Please do not remove the "Theme Memory designed by Art Chen" text and links.
 
 请不要删除页面底部的作者信息和链接。
 
--- source/js/app.js
+++ source/js/app.js
@@ -1,41 +1,76 @@
-var customSearch;
-(function($) {
-	
-	"use strict";
+(function ($) {
 
-  var scrolltoElement = function(e) {
+  "use strict";
+
+  var toggleActive = function (self, e) {
+    e.preventDefault();
+    if (self.hasClass("active") === true) {
+      self.removeClass("active");
+    } else {
+      self.addClass("active");
+    }
+  };
+
+  var switchSidebarTab = function (e) {
+    var self = $(this),
+      target = self.attr('data-toggle'),
+      counter_target = target === 'toc' ? 'bio' : 'toc';
+    if (self.hasClass('active')) {
+      return;
+    }
+    toggleActive(self, e);
+    toggleActive(self.siblings('.dark-btn'), e);
+    $('.site-' + counter_target).toggleClass('show');
+    setTimeout(function () {
+      $('.site-' + counter_target).hide();
+      $('.site-' + target).show();
+      setTimeout(function () {
+        $('.site-' + target).toggleClass('show');
+      }, 50);
+    }, 240);
+  };
+
+  var scrolltoElement = function (e) {
     e.preventDefault();
     var self = $(this),
-        correction = e.data ? e.data.correction ? e.data.correction : 0 : 0;
-    $('html, body').animate({'scrollTop': $(self.attr('href')).offset().top - correction }, 400);
+      correction = e.data ? e.data.correction ? e.data.correction : 0 : 0;
+    $('html, body').animate({'scrollTop': $(self.attr('href')).offset().top - correction}, 400);
   };
-	
-  var closeMenu = function(e) {
-	  e.stopPropagation();
+
+  var closeMenu = function (e) {
+    e.stopPropagation();
     $('body').removeClass('menu-open');
-		$('#site-nav-switch').removeClass('active');
+    $('#site-nav-switch').removeClass('active');
   };
-  
-  var toggleMenu = function(e) {
-	  e.stopPropagation();
-	  $('body').toggleClass('menu-open');
+
+  var toggleMenu = function (e) {
+    e.stopPropagation();
+    $('body').toggleClass('menu-open');
     $('#site-nav-switch').toggleClass('active');
   };
-	
-	$(function() {
-		$('#footer, #main').addClass('loaded');
-		$('#site-nav-switch').on('click', toggleMenu);
-		$(document).on('click', closeMenu);
-		$('#site-menu').on('click', function (e) {
-			e.stopPropagation();
-		});
-		$('.window-nav, .go-comment').on('click', scrolltoElement);
+
+  var pixivArchiveStat = function () {
+    var vol = $(".article-entry ul").length;
+    var artistCount = $(".article-entry ul li").length;
+    $("#pixiv-vol").text(vol);
+    $("#pixiv-artist-count").text(artistCount);
+  };
+
+  $(function () {
+    $('#footer, #main').addClass('loaded');
+    $('#site-nav-switch').on('click', toggleMenu);
+    $('#site-wrapper .overlay, #sidebar-close').on('click', closeMenu);
+    $('.window-nav, .site-toc a').on('click', scrolltoElement);
     $(".content .video-container").fitVids();
+    $('#site-sidebar .sidebar-switch .dark-btn').on('click', switchSidebarTab);
 
-		setTimeout(function() {
-	    $('#loading-bar-wrapper').fadeOut(500);
-	  }, 300);
+    if (window.location.pathname === '/pixiv' || window.location.pathname === '/pixiv/') {
+      pixivArchiveStat();
+    }
 
-	});
-		
+    setTimeout(function () {
+      $('#loading-bar-wrapper').fadeOut(500);
+    }, 300);
+  });
+
 })(jQuery);
